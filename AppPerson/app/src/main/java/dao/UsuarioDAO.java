27bfb62d78d5234ae20ffa3412926674ca23acc4
Matheus de Aguiar <102604133+MAguiarP@users.noreply.github.com>
2023-01-24 13:58:43 -0300

    add

--- AppPerson/app/src/main/java/dao/UsuarioDAO.java
+++ AppPerson/app/src/main/java/dao/UsuarioDAO.java
@@ -5,6 +5,9 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import model.Usuario;
 
 public class UsuarioDAO {
@@ -29,14 +32,24 @@ public class UsuarioDAO {
                 cursor.getString(cursor.getColumnIndex(DatabaseHelper.Usuarios.LOGIN)),
                 cursor.getString(cursor.getColumnIndex(DatabaseHelper.Usuarios.SENHA)),
                 cursor.getString(cursor.getColumnIndex(DatabaseHelper.Usuarios.CREATED_AT))
-
         );
+
         return model;
+    }
+
+    public List<Usuario> listarUsuarios(){
+        Cursor cursor = getDatabase().query(DatabaseHelper.Usuarios.TABELA,DatabaseHelper.Usuarios.COLUNAS, null, null, null, null, null);
+        List<Usuario> usuarios = new ArrayList<Usuario>();
+        while(cursor.moveToNext()){
+            Usuario model = criarUsuario(cursor);
+            usuarios.add(model);
+        }
+        cursor.close();
+        return usuarios;
     }
 
     public void fechar(){
         databaseHelper.close();
-        database = null ;
+        database = null;
     }
 }
-
